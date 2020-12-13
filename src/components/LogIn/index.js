63f5fe691bac7fa2dd08a414353250bbce4110d2
Danyil Kichalo <dkichalo@gmail.com>
2020-12-13 20:35:50 +0200

    change login component view depending on user login status

--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -5,12 +5,28 @@ import { ROUTES } from "../../assets";
 
 import styles from "./logIn.module.scss";
 
-export const LogIn = () => {
+export const LogIn = React.memo(() => {
   const history = useHistory();
 
+  const token = localStorage.getItem("token");
+  const userName = localStorage.getItem("userName");
+  const userPhoto = localStorage.getItem("userPhoto");
+
   return (
-    <p className={styles.logInText} onClick={() => history.push(ROUTES.login)}>
-      Вхід
-    </p>
+    <div className={styles.wrapper}>
+      {token ? (
+        <div className={styles.userContainer}>
+          <img src={userPhoto} alt="user" className={styles.userPhoto} />
+          <p className={styles.userNameText}>{userName}</p>
+        </div>
+      ) : (
+        <p
+          className={styles.logInText}
+          onClick={() => history.push(ROUTES.login)}
+        >
+          Вхід
+        </p>
+      )}
+    </div>
   );
-};
+});
